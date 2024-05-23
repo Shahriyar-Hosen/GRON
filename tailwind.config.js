9d30b5af0cebfa7e00cda27d8c6b571609a0e8ea
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-05-23 08:13:00 +0600

    setup tailwind css done

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/App.{js,jsx,ts,tsx}',
+    './src/components/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+    './src/navigation/**/*.{js,jsx,ts,tsx}',
+    './src/screens/**/*.{js,jsx,ts,tsx}',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
